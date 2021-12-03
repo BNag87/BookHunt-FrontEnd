67bdbc3fbc856e6bfb4cf1d441f8a564924bf488
shard520 <65344196+shard520@users.noreply.github.com>
2021-12-03 16:58:56 +0000

    Add theme provider and set fonts and colours.

--- src/Style.js
+++ src/Style.js
@@ -1,36 +1,32 @@
 //css style for a container div that contains text
 exports.Style = {
-    div:{
+  div: {
+    border: 'solid black 2px',
+    background: 'rgba(90,90,90,0.5)',
+    margin: '5px',
+    padding: '10px 10px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    justifyContent: 'space-evenly',
+  },
+};
 
-        border: "solid black 2px",
-        background: "rgba(90,90,90,0.5)",
-        margin: "5px",
-        padding: "10px 10px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-        justifyContent: "space-evenly",
-     },
-  }
-
-
-  //css style for a text box containing faq text
-  exports.textBoxStyle = {
-    div:{
-        border: "outset black 1.5px",
-        borderRadius: "8px",
-        background: "rgba(150,150,150,1)",
-        margin: "8px",
-        padding: "2px",
-        color: "white",
-        textAlign: "center",
-        fontSize: "medium",
-        fontFamily: "sans-serif",
-        boxShadow: "3px 3px 5px rgb(100,100,100)"
-    },
-    h3:{
-        color: "khaki",
-    },
- }
-
-
+//css style for a text box containing faq text
+exports.textBoxStyle = {
+  div: {
+    border: 'outset black 1.5px',
+    borderRadius: '8px',
+    background: 'rgba(150,150,150,1)',
+    margin: '8px',
+    padding: '2px',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 'medium',
+    fontFamily: "'Montserrat', sans-serif",
+    boxShadow: '3px 3px 5px rgb(100,100,100)',
+  },
+  h3: {
+    color: 'khaki',
+  },
+};
